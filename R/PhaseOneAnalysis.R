0a88157b346ac9b05e396bf57dc7ea2d777528df
GavDotZip <gavdotzip@gmail.com>
2024-02-19 19:45:08 +0000

    R commit

--- R/PhaseOneAnalysis.R
+++ R/PhaseOneAnalysis.R
@@ -19,3 +19,24 @@ if (!is.null(ph1TFdata)) {
 } else {
   cat("Error: Failed to load data from one or more files.\n")
 }
+
+
+# Print basic numeric calculations
+mean_dataTF <- sapply(ph1TFdata[, -1], mean) 
+median_dataTF <- sapply(ph1TFdata[, -1], median)
+sd_dataTF <- sapply(ph1TFdata[, -1], sd)
+
+# Round the results to 2 decimals
+mean_dataTF <- round(mean_dataTF, 2)
+median_dataTF <- round(median_dataTF, 2)
+sd_dataTF <- round(sd_dataTF, 2)
+
+# Combine results into a data frame
+analysis_results <- data.frame(
+  Mean = mean_dataTF,
+  Median = median_dataTF,
+  SD = sd_dataTF
+)
+
+# View the analysis results
+print(analysis_results)
