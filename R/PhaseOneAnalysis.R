714d668f957e54accc7fe02698c56159896d3178
GavDotZip <gavdotzip@gmail.com>
2024-02-19 19:36:28 +0000

    R test

--- R/PhaseOneAnalysis.R
+++ R/PhaseOneAnalysis.R
@@ -9,3 +9,13 @@ p1TradFixed <- "C:/Users/gavin/MscResearch/src/ph1/results/PhaseOneTraditionalFi
 
 # Print data structure
 ph1TFdata <- read_excel(p1TradFixed)
+
+if (!is.null(ph1TFdata)) {
+  
+  # Display summary of traditional data
+  cat("\nSummary of Phase One Traditional Data:\n")
+  print(summary(ph1TFdata))
+  
+} else {
+  cat("Error: Failed to load data from one or more files.\n")
+}
